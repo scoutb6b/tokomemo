5346ef7b3e7b914c3ee071b00d325e5e7d36311c
scoutb6b <scoutboq@gmail.com>
2024-11-27 13:45:33 +0900

    token検証

--- src/app/api/store/[id]/route.ts
+++ src/app/api/store/[id]/route.ts
@@ -1,27 +1,56 @@
 import prisma from "@/_libs/prisma";
+import { supabase } from "@/_libs/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (
   req: NextRequest,
   { params }: { params: { id: string } }
 ) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+  console.log(token);
+
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
+
   const { id } = params;
+  console.log("id", id);
+  console.log("supa", supabaseUserid);
+
   try {
     const store = await prisma.store.findUnique({
       where: {
         id,
+        user: {
+          supabaseUserid,
+        },
       },
+      // select: {
+      //   name: true,
+      // },
     });
+    console.log(store);
+
     return NextResponse.json({ store, message: "取得おk" }, { status: 200 });
   } catch (error) {
     if (error instanceof Error)
-      return NextResponse.json({ message: error.message }, { status: 400 });
+      return NextResponse.json({ message: error.message }, { status: 401 });
   }
 };
 export const PUT = async (
   req: NextRequest,
   { params }: { params: { id: string } }
 ) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
+
   const { id } = params;
   console.log(id);
 
@@ -33,6 +62,9 @@ export const PUT = async (
     const storeName = await prisma.store.update({
       where: {
         id,
+        user: {
+          supabaseUserid,
+        },
       },
       data: {
         name: store,
@@ -44,18 +76,28 @@ export const PUT = async (
     );
   } catch (error) {
     if (error instanceof Error)
-      return NextResponse.json({ message: error.message }, { status: 400 });
+      return NextResponse.json({ message: error.message }, { status: 401 });
   }
 };
 export const DELETE = async (
   req: NextRequest,
   { params }: { params: { id: string } }
 ) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
   const { id } = params;
   try {
     await prisma.store.delete({
       where: {
         id,
+        user: {
+          supabaseUserid,
+        },
       },
     });
     return NextResponse.json({ message: "store削除完了" }, { status: 200 });
