b808b12604ed252fc383fad03ae46eb9b1e4fb73
scoutb6b <scoutboq@gmail.com>
2024-11-29 23:18:10 +0900

    カテゴリーの作成、一覧　一件ごとの取得、更新、削除のapiを作成

---
+++ src/app/api/category/[id]/route.ts
@@ -0,0 +1,99 @@
+import prisma from "@/_libs/prisma";
+import { supabase } from "@/_libs/supabase";
+import { NextRequest, NextResponse } from "next/server";
+
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
+  const { id } = params;
+  try {
+    const category = await prisma.category.findUnique({
+      where: {
+        id,
+        user: {
+          supabaseUserid,
+        },
+      },
+    });
+    return NextResponse.json(
+      { category, message: "カテゴリー取得完了" },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 401 });
+    }
+  }
+};
+export const PUT = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
+
+  const { id } = params;
+
+  try {
+    const body = await req.json();
+    const { category } = body;
+
+    const categoryName = await prisma.category.update({
+      where: {
+        id,
+        user: {
+          supabaseUserid,
+        },
+      },
+      data: {
+        name: category,
+      },
+    });
+    return NextResponse.json(
+      { message: "category更新完了", category: categoryName },
+      { status: 200 }
+    );
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 401 });
+  }
+};
+export const DELETE = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
+  const { id } = params;
+  try {
+    await prisma.category.delete({
+      where: {
+        id,
+        user: {
+          supabaseUserid,
+        },
+      },
+    });
+    return NextResponse.json({ message: "category削除完了" }, { status: 200 });
+  } catch (error) {
+    if (error instanceof Error)
+      return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+};
