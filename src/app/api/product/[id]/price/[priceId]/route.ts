25b153ed7e986cad758db99334bc59c130a8a245
scoutb6b <scoutboq@gmail.com>
2025-01-16 09:59:33 +0900

    priceid パラメーターのGETを追記

--- src/app/api/product/[id]/price/[priceId]/route.ts
+++ src/app/api/product/[id]/price/[priceId]/route.ts
@@ -2,6 +2,47 @@ import prisma from "@/app/_libs/prisma";
 import { supabase } from "@/app/_libs/supabase";
 import { NextRequest, NextResponse } from "next/server";
 
+export const GET = async (
+  req: NextRequest,
+  { params }: { params: Promise<{ priceId: string }> }
+) => {
+  const token = req.headers.get("Authorization") ?? "";
+  const { data, error } = await supabase.auth.getUser(token);
+
+  if (error) {
+    return NextResponse.json({ message: error.message }, { status: 400 });
+  }
+  const supabaseUserid = data.user?.id;
+  const { priceId } = await params;
+  const user = await prisma.user.findUnique({
+    where: {
+      supabaseUserid,
+    },
+  });
+
+  if (!user) {
+    return NextResponse.json(
+      { message: "登録しているユーザーから見つかりませんでした" },
+      { status: 404 }
+    );
+  }
+  try {
+    const data = await prisma.price.findMany({
+      where: {
+        id: priceId,
+        store: {
+          userId: user.id,
+        },
+      },
+    });
+    return NextResponse.json(data);
+  } catch (error) {
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 401 });
+    }
+  }
+};
+
 export const PUT = async (
   req: NextRequest,
   { params }: { params: Promise<{ priceId: string }> }
