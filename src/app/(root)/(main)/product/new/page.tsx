cefd15bc7ba020402b3c5a714bd42a3e7e72b0ca
scoutb6b <scoutboq@gmail.com>
2025-01-17 10:39:22 +0900

    商品新規追加ページ

---
+++ src/app/(root)/(main)/product/new/page.tsx
@@ -0,0 +1,88 @@
+"use client";
+import { NativeSelect, TextInput } from "@mantine/core";
+import { AddButton } from "../_components/AddButton";
+import { useFetch } from "@/app/_hooks/useFetch";
+import { Category } from "@/app/_types/ApiResponse/Category";
+import { FormEvent, useState } from "react";
+import { useSupabaseSession } from "@/app/_hooks/useSupabaseSession";
+import { notifications } from "@mantine/notifications";
+import { NextPage } from "next";
+
+type CategorySelect = Pick<Category, "id" | "name">;
+
+const ProductNewPage: NextPage = () => {
+  const { token } = useSupabaseSession();
+  const [product, setProduct] = useState("");
+  const [categoryId, setCategoryId] = useState("");
+
+  const { data: cateogries } = useFetch<Category[]>("/api/category");
+
+  const categoryArr = cateogries?.map((category: CategorySelect) => {
+    return { value: category.id, label: category.name };
+  });
+
+  const categorySelect = [
+    { value: "", label: "カテゴリなし" },
+    ...(categoryArr ?? []),
+  ];
+
+  const clickCreate = async (e: FormEvent<HTMLFormElement>) => {
+    if (!token) return;
+
+    e.preventDefault();
+    try {
+      await fetch(`${process.env.NEXT_PUBLIC_APP_BASE_URL}/api/product`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: token,
+        },
+        body: JSON.stringify({ product, categoryId }),
+      });
+      notifications.show({
+        title: "商品の登録が完了しました",
+        message: "",
+        autoClose: 2500,
+        position: "bottom-right",
+        color: "green",
+      });
+    } catch (error) {
+      notifications.show({
+        title: "エラーが発生しました",
+        message: `${error}`,
+        autoClose: 2500,
+        position: "bottom-right",
+        color: "red",
+      });
+    }
+  };
+
+  return (
+    <div>
+      <h1>商品追加</h1>
+      <div>
+        <form onSubmit={clickCreate}>
+          <TextInput
+            size="md"
+            radius="md"
+            label="商品名"
+            value={product}
+            onChange={(e) => setProduct(e.currentTarget.value)}
+          />
+          <NativeSelect
+            size="md"
+            radius="md"
+            mt="lg"
+            label="カテゴリー"
+            value={categoryId}
+            onChange={(e) => setCategoryId(e.currentTarget.value)}
+            data={categorySelect}
+          />
+          <AddButton />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ProductNewPage;
