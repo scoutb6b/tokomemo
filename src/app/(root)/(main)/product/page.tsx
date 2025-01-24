3d61d554e3ff59614a051fa33e45c2931df17f1f
scoutb6b <scoutboq@gmail.com>
2025-01-24 16:08:47 +0900

    データ空の場合のreturnを変更

---
+++ src/app/(root)/(main)/product/page.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@mantine/core";
+import { AddIcon } from "./_components/AddIcon";
+import { Tab } from "./_components/Tab";
+
+const ProductPage = () => {
+  return (
+    <Box mb={80}>
+      <Tab />
+
+      <AddIcon />
+    </Box>
+  );
+};
+
+export default ProductPage;
