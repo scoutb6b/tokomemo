9055412537b89d1b56060ea14e2e2dc6bf30f4b5
scoutb6b <scoutboq@gmail.com>
2025-02-17 22:22:14 +0900

    新規登録、ログイン時の軽微な修正

--- src/app/(root)/(auth)/signup/page.tsx
+++ src/app/(root)/(auth)/signup/page.tsx
@@ -3,15 +3,19 @@
 import { authSchema } from "@/app/_libs/zod/schema";
 import { createUser } from "./action";
 import {
+  Anchor,
   Box,
   Button,
   Group,
   PasswordInput,
   TextInput,
   Title,
+  Text,
 } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { zodResolver } from "mantine-form-zod-resolver";
+import { notifications } from "@mantine/notifications";
+import { FormEvent } from "react";
 
 const SignUpPage = () => {
   const form = useForm({
@@ -22,34 +26,51 @@ const SignUpPage = () => {
     validate: zodResolver(authSchema),
   });
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
+  const handleSubmit = async (
+    _value: typeof form.values,
+    e: FormEvent<HTMLFormElement> | undefined
+  ) => {
+    e?.preventDefault();
     if (form.validate().hasErrors) {
       console.error("バリデーションエラー");
       return;
     }
     const { mail, password } = form.getValues();
+
     try {
       await createUser(mail, password);
-      alert("認証メールを送信しました");
+      notifications.show({
+        title: "メールが送信されましたので、ご確認ください",
+        message: "右のXボタンにて閉じてください。",
+        autoClose: false,
+        color: "green",
+        position: "top-center",
+      });
+      form.reset();
     } catch (error) {
-      console.error(error);
+      notifications.show({
+        title: "登録できませんでした",
+        message: "",
+        autoClose: 10000,
+        color: "red",
+        position: "top-center",
+      });
     }
   };
 
   return (
     <Box w="94%" mx="auto">
       <Title size="h2">新規登録</Title>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={form.onSubmit(handleSubmit)}>
         <TextInput
           mt={20}
           label="メールアドレス"
           type="email"
           name="email"
           id="email"
-          required
           {...form.getInputProps("mail")}
           error={form.errors.mail}
+          disabled={form.submitting}
         />
 
         <PasswordInput
@@ -58,9 +79,10 @@ const SignUpPage = () => {
           type="password"
           name="password"
           id="password"
-          required
+          description="6文字以上必須"
           {...form.getInputProps("password")}
           error={form.errors.password}
+          disabled={form.submitting}
         />
 
         <Group justify="center" mt="md">
@@ -71,11 +93,17 @@ const SignUpPage = () => {
             px="xl"
             size="md"
             radius="md"
+            loading={form.submitting}
           >
             登録
           </Button>
         </Group>
       </form>
+      <Box mt={40}>
+        <Anchor href="/login" underline="always">
+          <Text ta="center">ログインページはこちら</Text>
+        </Anchor>
+      </Box>
     </Box>
   );
 };
