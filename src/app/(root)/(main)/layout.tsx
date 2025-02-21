fd10477e51cc88f46cd19fc1273c4dad8847972c
scoutb6b <scoutboq@gmail.com>
2025-02-21 21:48:37 +0900

    build err

--- src/app/(root)/(main)/layout.tsx
+++ src/app/(root)/(main)/layout.tsx
@@ -4,13 +4,14 @@ import { useRouteGuard } from "@/app/_hooks/useRouteGuard";
 import MainLayout from "@/app/layouts";
 import PullToRefresh from "react-simple-pull-to-refresh";
 
-const handleRefresh = async () => {
-  window.location.reload();
-};
-const isPWA = window.matchMedia("(display-mode: standalone)").matches;
-
 const Layout = ({ children }: { children: React.ReactNode }) => {
   useRouteGuard();
+  const handleRefresh = async () => {
+    window.location.reload();
+  };
+  const isPWA =
+    typeof window !== "undefined" &&
+    window.matchMedia("(display-mode: standalone)").matches;
 
   return isPWA ? (
     <PullToRefresh onRefresh={handleRefresh}>
