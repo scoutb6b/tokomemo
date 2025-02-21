54df300bca15162b9d4dc3895a3a75fbab547f0a
scoutb6b <scoutboq@gmail.com>
2025-02-21 21:27:55 +0900

    pwa環境のための下スワイプリロードを実装

--- src/app/(root)/(main)/layout.tsx
+++ src/app/(root)/(main)/layout.tsx
@@ -2,11 +2,23 @@
 
 import { useRouteGuard } from "@/app/_hooks/useRouteGuard";
 import MainLayout from "@/app/layouts";
+import PullToRefresh from "react-simple-pull-to-refresh";
+
+const handleRefresh = async () => {
+  window.location.reload();
+};
+const isPWA = window.matchMedia("(display-mode: standalone)").matches;
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   useRouteGuard();
 
-  return <MainLayout>{children}</MainLayout>;
+  return isPWA ? (
+    <PullToRefresh onRefresh={handleRefresh}>
+      <MainLayout>{children}</MainLayout>
+    </PullToRefresh>
+  ) : (
+    <MainLayout>{children}</MainLayout>
+  );
 };
 
 export default Layout;
