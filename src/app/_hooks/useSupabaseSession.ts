2a58ce537dcca5d6d967f5bf9e90c8bd5fb35362
scoutb6b <scoutboq@gmail.com>
2025-02-24 22:44:29 +0900

    token管理を変更

--- src/app/_hooks/useSupabaseSession.ts
+++ src/app/_hooks/useSupabaseSession.ts
@@ -1,11 +1,14 @@
 import { supabase } from "@/app/_libs/supabase";
 import { Session } from "@supabase/supabase-js";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export const useSupabaseSession = () => {
   const [session, setSession] = useState<Session | null | undefined>(undefined);
   const [token, setToken] = useState<string | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+
+  const pathbane = usePathname();
 
   useEffect(() => {
     const fetcher = async () => {
@@ -17,6 +20,6 @@ export const useSupabaseSession = () => {
       setIsLoading(false);
     };
     fetcher();
-  }, []);
+  }, [pathbane]);
   return { session, token, isLoading };
 };
